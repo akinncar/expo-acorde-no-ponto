bbd2a50462ce735cefa6be95ad019dac11334d7d
akinncar <akinncar@hotmail.com>
2021-08-29 14:57:41 -0300

    feat(destination): fetch background location

--- src/components/Home/Route.js
+++ src/components/Home/Route.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import {GOOGLE_MAPS_API_KEY} from '@env'
 import MapView from "react-native-maps";
 import MapViewDirections from 'react-native-maps-directions';
 import * as Location from 'expo-location';
-import {GOOGLE_MAPS_API_KEY} from '@env'
+import * as TaskManager from "expo-task-manager";
 
 export function Route({route, navigation}) {
     const { destination } = route.params;
 
     const [location, setLocation] = React.useState(null);
     const [errorMsg, setErrorMsg] = React.useState(null);
+
+    const TASK_FETCH_LOCATION = 'TASK_FETCH_LOCATION';
+
+    TaskManager.defineTask(TASK_FETCH_LOCATION, async ({ data: { locations }, error }) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        const [location] = locations;
+        try {
+            console.log(location)
+            setLocation(location);
+        } catch (err) {
+            console.error(err);
+        }
+    });
 
     React.useEffect(() => {
         (async () => {
@@ -18,9 +35,15 @@ export function Route({route, navigation}) {
                 return;
             }
 
-            await Location.watchPositionAsync({accuracy: Location.Accuracy.High}, (location) => {
-                console.log(location)
-                setLocation(location);
+            await Location.startLocationUpdatesAsync(TASK_FETCH_LOCATION, {
+                accuracy: Location.Accuracy.Highest,
+                distanceInterval: 1, // minimum change (in meters) betweens updates
+                deferredUpdatesInterval: 1000, // minimum interval (in milliseconds) between updates
+                // foregroundService is how you get the task to be updated as often as would be if the app was open
+                foregroundService: {
+                    notificationTitle: 'Using your location',
+                    notificationBody: 'To turn off, go back to the app and switch something off.',
+                },
             });
         })();
     }, []);
